6f74b232e300bc861e8042d038ceb197a9255efb
c1chy1 <cichy08081987pp@gmail.com>
2023-12-07 17:29:23 +0100

    banner add\  #11

---
+++ components/atoms/Banner.vue
@@ -0,0 +1,35 @@
+
+<template>
+<article class="relative md:h-[50%-35px] mt-[35px] overflow-hidden">
+<div class="absolute top-0 left-0 text-white ml-8 mt-7 z-[1]">
+  <h4 class="text-[25px] font-medium font-bebas leading-normal">{{subject}}</h4>
+  <h3 class="text-[42px] font-medium" >{{ title }}</h3>
+
+</div>
+
+        <img :src="src"
+             class="
+             hover:scale-110
+             transition
+             duration-500
+             cursor-pointer
+             object-cover">
+
+</article>
+
+
+
+</template>
+<script setup lang="ts">
+
+defineProps({
+  subject: String,
+  title: String,
+  src : String
+})
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/layout/Home.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+
+  <div class="px-20 py-10    grid grid-cols-1  md:grid-cols-[0.6fr 0.5fr] md:grid-rows-2 justify-center items-center md:gap-x-12 md:gap-y-6">
+    <div class="area ">
+      <AtomsBanner
+          src="images/home/1.png"
+          subject="Try it today"
+          title="Most popular kebab"
+      />
+    </div>
+    <div class="area-2">
+      <AtomsBanner
+          src="images/home/2.png"
+          subject="Try it today"
+          title="More fun more taste"
+      />
+    </div>
+    <div class="area-3">
+      <AtomsBanner
+          src="images/home/3.png"
+          subject="Try it today"
+          title="Fresh & Chili"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.area {
+  grid-area: 1 / 1 / 3 / 2;
+}
+
+.area-2 {
+  grid-area: 1 / 2 / 2 / 3;
+}
+
+.area-3 {
+  grid-area: 2 / 2 / 3 / 3;
+}
+
+
+</style>
