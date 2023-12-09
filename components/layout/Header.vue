aa37ac1910c8fa91baba48e123b6bb71ef8dde5b
c1chy1 <cichy08081987pp@gmail.com>
2023-12-09 21:03:43 +0100

    add mobil test  #15

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -43,13 +43,14 @@ const kebabs = reactive([
 <!--            <div
                 class=" absolute overflow-hidden flex items-center h-[104px] w-[104px] min-w-[104px]   md:h-[160px] md:min-w-[160px] top-[40px]  md:bottom-[80px] md:right-[90px]  p-2 md:p-2.5  bg-[#ff6d00] rounded-full  object-cover">
 
-              <article
-                  class="relative flex flex-col items-center justify-center w-full h-full p-[10px] text-[30px]  md:text-[60px]  md:leading-[80px] border-2 border-dashed border-white  rounded-full text-white font-base font-bebas  transform rotate-[20deg]">
-
-                20% <span class="  text-[24px] md:text-[33px] leading-[20px]">Off</span></article>
 
             </div>-->
-            <div class="   xl:text-left  flex flex-col items-start">
+                  <article
+                      class="relative flex flex-col items-center justify-center w-full h-full p-[10px] text-[30px]  md:text-[60px]  md:leading-[80px] border-2 border-dashed border-white  rounded-full text-white font-base font-bebas  transform rotate-[20deg]">
+
+                    20% <span class="  text-[24px] md:text-[33px] leading-[20px]">Off</span></article>
+
+                  <div class="   xl:text-left  flex flex-col items-start">
               <h3 class=" w-full  py-0  sm:py-1  lg:w-auto   lg:px-4 lg:py-2 mb-4  text-[14px] lg:text-[16px]  xl:text-[24px] text-primary border-primary font-medium font-bebas border-dashed border-2 inline-block rounded-[3px] ">
                 It is a good time for <br class="sm:hidden"> the great taste of kebabs
               </h3>
