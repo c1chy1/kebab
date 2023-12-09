df3d12edfb01b526fce18712a8dd1ecaad9b0b04
c1chy1 <cichy08081987pp@gmail.com>
2023-12-09 21:10:10 +0100

    add mobil test  #15

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -38,17 +38,15 @@ const kebabs = reactive([
                 <div
                     class="relative flex flex-col items-center text-center lg:text-left lg:justify-between   lg:flex-row-reverse uppercase "
                 ><img
-                    :src="slide"
-              class="logo-box w-4/5 md:w-1/2 object-scale-down"/>
-<!--            <div
-                class=" absolute overflow-hidden flex items-center h-[104px] w-[104px] min-w-[104px]   md:h-[160px] md:min-w-[160px] top-[40px]  md:bottom-[80px] md:right-[90px]  p-2 md:p-2.5  bg-[#ff6d00] rounded-full  object-cover">
+                    :src="slide" class="logo-box w-4/5 md:w-1/2 object-scale-down"/>
+            <div class="hidden absolute overflow-hidden md:flex items-center h-[104px] w-[104px] min-w-[104px]   md:h-[160px] md:min-w-[160px] top-[40px]  md:bottom-[80px] md:right-[90px]  p-2 md:p-2.5  bg-[#ff6d00] rounded-full  object-cover">
 
-
-            </div>-->
-                  <article
+            <article
                       class="relative flex flex-col items-center justify-center w-full h-full p-[10px] text-[30px]  md:text-[60px]  md:leading-[80px] border-2 border-dashed border-white  rounded-full text-white font-base font-bebas  transform rotate-[20deg]">
 
                     20% <span class="  text-[24px] md:text-[33px] leading-[20px]">Off</span></article>
+            </div>
+
 
                   <div class="   xl:text-left  flex flex-col items-start">
               <h3 class=" w-full  py-0  sm:py-1  lg:w-auto   lg:px-4 lg:py-2 mb-4  text-[14px] lg:text-[16px]  xl:text-[24px] text-primary border-primary font-medium font-bebas border-dashed border-2 inline-block rounded-[3px] ">
