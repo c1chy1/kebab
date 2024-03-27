33992fab63bc44f7e0af1b60853c869b5c532840
c1chy1 <cichy08081987pp@gmail.com>
2024-03-27 19:42:57 +0100

    mobile nav fix

--- components/atoms/Banner.vue
+++ components/atoms/Banner.vue
@@ -2,8 +2,8 @@
 <template>
 <article class="relative  w-full overflow-hidden rounded-xl">
 <div class="absolute top-0 left-0 text-white ml-3 mt-2 lg:ml-8 lg:mt-7 z-[1]">
-  <h4 class="  text-sm lg:text-[25px] font-medium font-bebas leading-normal">{{subject}}</h4>
-  <h3 class=" text-xl lg:text-[42px] font-medium" >{{ title }}</h3>
+  <h4 class="  text-sm sm:text-xl lg:text-[25px] font-medium font-bebas leading-normal">{{subject}}</h4>
+  <h3 class=" text-xl sm:text-4xl lg:text-[42px] font-medium" >{{ title }}</h3>
 
 </div>
 
